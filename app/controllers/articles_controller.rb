e23214460635be0e4d5969de14012293ac27e657
Arnaud Rioche <arnaud.rioche@icloud.com>
2019-08-15 10:14:09 +0200

    fexed dates seed and article ordering in #articles and #pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all.order(id: :desc)
+    @articles = Article.all.order(date: :desc)
   end
 
   def show
