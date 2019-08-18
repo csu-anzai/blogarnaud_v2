f83e48017adb861171ae38b60dfb0fea0bc60715
Arnaud Rioche <arnaud.rioche@icloud.com>
2019-08-18 16:31:56 +0200

    added validations to entreprise model

--- app/models/entreprise.rb
+++ app/models/entreprise.rb
@@ -1,3 +1,14 @@
 class Entreprise < ApplicationRecord
   belongs_to :tag
+
+  validates :name, presence: true
+  validates :value_proposition_fr, presence: true
+  validates :content_fr, presence: true
+  validates :photo, presence: true
+  validates :logo, presence: true
+
+  has_rich_text :value_proposition_fr
+  has_rich_text :value_proposition_en
+  has_rich_text :content_fr
+  has_rich_text :content_en
 end
