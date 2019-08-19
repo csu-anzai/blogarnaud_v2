6931f75439e05ce4c4204b7bf1159e3abc6a4a64
Arnaud Rioche <arnaud.rioche@icloud.com>
2019-08-19 17:46:43 +0200

    deleted title, added title_fr and title_en to db schema for articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_165553) do
+ActiveRecord::Schema.define(version: 2019_08_19_154527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2019_08_18_165553) do
   end
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
     t.date "date"
     t.string "photo"
     t.text "content_fr"
@@ -55,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_08_18_165553) do
     t.bigint "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title_fr"
+    t.string "title_en"
     t.index ["tag_id"], name: "index_articles_on_tag_id"
   end
 
